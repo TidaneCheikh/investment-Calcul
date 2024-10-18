f8dc9064f8f88d7d208a0f0ff04197b952e69846
cheikhtidiane <cheikhtidiane15.thiam@ucad.edu.sn>
2024-10-18 15:01:56 +0000

    The services

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,6 +1,7 @@
-import { Component ,Input , EventEmitter, Output} from '@angular/core';
+import { Component ,Input , EventEmitter, Output, inject} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -11,17 +12,18 @@ import { InvestmentInput } from '../investment-input.model';
 })
 export class UserInputComponent {
   @Output() calculate = new EventEmitter<InvestmentInput>();
+  invest =inject(InvestmentService)
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
 
   onSubmit() {
-    this.calculate.emit({
+    this.invest.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment,
       duration: +this.enteredDuration,
       expectedReturn: +this.enteredExpectedReturn,
       annualInvestment: +this.enteredAnnualInvestment,
-    });
+    })
   }
 }
